5b3379dbbf589cb3f6d770dc9061f50fcc0d0240
Fernando <feh.wilinando@gmail.com>
2019-06-16 23:32:27 -0300

    Create project

---
+++ src/main/java/br/com/caelum/fj36/rest/RestApplication.java
@@ -0,0 +1,13 @@
+package br.com.caelum.fj36.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+
+}
